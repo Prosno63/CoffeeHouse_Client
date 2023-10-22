c0b578b87bb89d03e22dde8763d0a8c164a59146
Istiak38 <listiakahamad63@gmail.com>
2023-10-22 13:06:59 -0700

    Installations

---
+++ coffee-store-client/firebse/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDf6sVqSl5RNVZkKxxY6yth7C2VlYzTX18",
+  authDomain: "coffeetea-19503.firebaseapp.com",
+  projectId: "coffeetea-19503",
+  storageBucket: "coffeetea-19503.appspot.com",
+  messagingSenderId: "184264686091",
+  appId: "1:184264686091:web:928a08e3a9a41c228be50e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
